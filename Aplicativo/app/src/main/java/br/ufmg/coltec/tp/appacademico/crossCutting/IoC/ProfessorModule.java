91c843c0d686173d30e9a252142d573b43eda473
Germano Barcelos <germanobarcelos@yahoo.com.br>
2018-09-28 20:33:31 -0300

    IoC Implemented

--- Aplicativo/app/src/main/java/br/ufmg/coltec/tp/appacademico/crossCutting/IoC/ProfessorModule.java
+++ Aplicativo/app/src/main/java/br/ufmg/coltec/tp/appacademico/crossCutting/IoC/ProfessorModule.java
@@ -1,27 +1,12 @@
 package br.ufmg.coltec.tp.appacademico.crossCutting.IoC;
 
 import br.ufmg.coltec.tp.appacademico.controller.ProfessorController;
-import br.ufmg.coltec.tp.appacademico.data.Database;
-import br.ufmg.coltec.tp.appacademico.data.interfaces.IProfessorRepository;
-
 import br.ufmg.coltec.tp.appacademico.service.FachadaProfessor;
 import dagger.Module;
 import dagger.Provides;
 
 @Module
 public class ProfessorModule {
-
-/* Comentei pq o metodo de get ta comentado tbm, tava dando algum erro de conflito */
-
-//    @Provides
-//    public IProfessorRepository providesProfessorRepository(Database db) {
-//        return db.getProfessorRepository();
-//    }
-
-    @Provides
-    public ProfessorController provideProfessorController(IProfessorRepository repository) {
-        return new ProfessorController(repository);
-    }
 
     @Provides
     public FachadaProfessor provideFachadaProfessor(ProfessorController controller) {
